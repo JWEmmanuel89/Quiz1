98ecb19398d33fda88d7ea11b6edce57766caa42
JWEmmanuel89 <90356423+JWEmmanuel89@users.noreply.github.com>
2023-01-29 14:48:54 -0600

    Question 5

--- main.go
+++ main.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"fmt"
 	"math"
 )
 
@@ -17,16 +18,20 @@ func (t Triangle) Area() float64 {
 	return (t.Base * t.Height) / 2
 }
 
-// #2. Create method type Perimeter
+// #3. Create method type Perimeter
 func (t Triangle) Perimeter() float64 {
 	sideC := math.Sqrt((t.Base * t.Base) + (t.Height * t.Height))
 	return t.Base + t.Height + sideC
 }
 
 func main() {
-	// #3. Create variable of type Triangle
+	// #4. Create variable of type Triangle
 	tri1 := Triangle{
 		Base:   2,
 		Height: 5,
 	}
+
+	//#5. Call Area and Perimeter of Triangle
+	fmt.Println(tri1.Area())
+	fmt.Println(tri1.Perimeter())
 }
